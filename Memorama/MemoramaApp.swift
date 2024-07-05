0b26e8ec22a9a38dd314979b12eaa9439dca5dd3
Alejandro Pallares <bizzlee200@gmail.com>
2024-07-04 19:36:43 -0600

    Initial Commit

---
+++ Memorama/MemoramaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoramaApp.swift
+//  Memorama
+//
+//  Created by Alejandro Pallares on 04/07/24.
+//
+
+import SwiftUI
+
+@main
+struct MemoramaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
