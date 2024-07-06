0f0b78cc60035efe1c0aacd9376e12b935d40879
Alejandro Pallares <alejandropallares10gmail.com>
2024-07-05 20:54:35 -0600

    Fiinished RT1 of Assigment 1

--- Memorama/ContentView.swift
+++ Memorama/ContentView.swift
@@ -8,15 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
-    let emojis = ["👻", "🎃", "🦠", "☠️"]
+    let emojis = ["👻", "🎃", "🦠", "☠️","👻","🎃", "🦠", "☠️", ]
+   @State var countCard = 4
     var body: some View {
-        HStack {
-            ForEach(emojis.indices, id: \.self) { index in
-                CardView(content: "👻")
+        VStack {
+            ScrollView {
+                cards
             }
+          cardAdjusters
+        }
+       
+    }
+    
+    var cards: some View {
+        HStack {
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
+                ForEach(0..<countCard, id: \.self) { index in
+                    CardView(content: "\(emojis[index])")
+                        .aspectRatio(2/3, contentMode: .fit)
+                }
+            }
+            
         }
         .padding()
-        .foregroundColor(.orange)
+    .foregroundColor(.orange)
+    }
+    
+    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
+        Button(action: {
+            countCard += offset
+        }, label: {
+           Image(systemName: symbol)
+        })
+        .disabled(countCard + offset < 1 || countCard + offset > emojis.count)
+    }
+    
+    var cardRemover: some View {
+        cardCountAdjuster(by: -1, symbol: "rectangle.stack.badge.minus.fill")
+    }
+    
+    var cardAdder: some View {
+        cardCountAdjuster(by: +1, symbol: "rectangle.stack.badge.plus.fill")
+    }
+    var cardAdjusters: some View {
+        HStack {
+            cardRemover
+            Spacer()
+            cardAdder
+        }
+        .imageScale(.large)
+        .font(.largeTitle)
+        .padding()
     }
     
     struct CardView: View {
@@ -25,14 +67,15 @@ struct ContentView: View {
         var body: some View {
             ZStack {
                 let base = RoundedRectangle(cornerRadius: 10)
-                if isFaceUp {
+                Group {
                     base.fill(.white)
                     base.strokeBorder(lineWidth: 2)
                     
                     Text(content).font(.largeTitle)
-                } else {
-                    base.fill(.orange)
+                    
                 }
+                .opacity(isFaceUp ? 1 : 0)
+                base.fill(.orange).opacity(isFaceUp ? 0 : 1)
                 
             }
             
